1cb63f9a20ac55ad72b778dda62e83b7ce3deb8f
Matthew Kowalski <24287049+matt325@users.noreply.github.com>
2019-07-08 12:17:40 -0400

    CoreML Intro

---
+++ CoreML Intro/README.md
@@ -0,0 +1,7 @@
+# Introduction to CoreML
+
+---
+## Purpose
+As a way to learn the fundamentals of CoreML and what it has to offer, I am following the creation of the "SeeFood" app from the Udemy course.
+
+Follows the [Udemy Course](https://www.udemy.com/ios-12-app-development-bootcamp/)
---
+++ CoreML Intro/SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Matt on 7/8/19.
+//  Copyright © 2019 Matthew Kowalski. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision   // Helps us process images easier and allows us to use images that work with CoreML without having to write a lot more code
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
